d1aee3a172fd963d179a9f81a55248d631bae328
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-11-05 15:48:56 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "@choose-tech/astro/config";
+
+export default defineConfig({
+  name: "template",
+  title: "Template",
+  description: "",
+  color: "#3b82f6",
+  repo: "choose-tech/template",
+  logoFilename: "logo.svg",
+});
---
+++ content/config.mjs
@@ -0,0 +1,1 @@
+export { collections } from "@choose-tech/astro/collections";
